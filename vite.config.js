ba34f638b0e0f8be7222849bc4ecd19af4b67dab
amirhosseim1365 <amir.h.moharreri@gmail.com>
2025-06-01 17:55:26 +0330

    Initial deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/charisma_app/',
   server: {
     allowedHosts: [
       'localhost',
